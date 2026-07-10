1c48b5691409a6476e3fe6a2b8e658309486415d
doublepice1 <doublepice1@gituhub.com>
2026-07-10 12:24:04 +0900

    うおｗ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float speedMax;
+    [SerializeField] float groundNormalYMin = 0.7f;
+    [SerializeField] float groundDamping = 8f;
+    [SerializeField] float airDamping = 0.5f;
+    [SerializeField] float jumpSpeed;
+
+    bool isGrounded;
+
+    private Rigidbody rb;
 
     PlayerInput playerInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        rb.sleepThreshold = -1;
     }
 
-    // Update is called once per frame
     void Update()
     {
         var moveVec = playerInput.actions["Move"].ReadValue<Vector2>();
@@ -28,5 +36,35 @@ public class Player : MonoBehaviour
             cameraDir * moveVec.y * speedMax
             + cameraRight * moveVec.x * speedMax;
         transform.position = transform.position + moveVec3D * Time.deltaTime;
+
+        if (playerInput.actions["Jump"].WasPressedThisFrame()
+   && isGrounded)
+        {
+            Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
+            rb.AddForce(jumpVec, ForceMode.VelocityChange);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            rb.linearDamping = groundDamping;
+        }
+        else
+        {
+            rb.linearDamping = airDamping;
+        }
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        foreach (var contact in collision.contacts) 
+        {
+            if (contact.normal.y >= groundNormalYMin)
+            {
+                isGrounded = true;
+            }
+        }
     }
 }
